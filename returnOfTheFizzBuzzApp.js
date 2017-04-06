2da4a2f4c3358faaa391e0d2bb56256b80c9fee0
vcordero07 <vcordero07@gmail.com>
2017-04-05 22:48:34 -0400

    refactor of the code

--- returnOfTheFizzBuzzApp.js
+++ returnOfTheFizzBuzzApp.js
@@ -1,5 +1,9 @@
 // your code here
-//04/03/17
+//04/05/17
+const strFizzBuzz = 'fizzbuzz';
+const strBuzz = 'buzz';
+const strFizz = 'fizz';
+
 
 let getInputValue = () => {
     return $('#number-choice').val();
@@ -15,36 +19,55 @@ let getRange = num => {
     return new Array(num).fill(0).map((el, ind) => ind + 1);
 };
 
+// let getFizzBuzzRange = arr => {
+//
+//     return arr.map((el, ind) => {
+//         if (el % 15 === 0) {
+//             return "fizzbuzz";
+//         } else if (el % 5 === 0) {
+//             return "buzz";
+//         } else if (el % 3 === 0) {
+//             return "fizz";
+//         } else {
+//             return el;
+//         }
+//     });
+// };
+//
+// let pushFizzBuzzHTML = arr => {
+//     return arr.map((el, ind) => {
+//
+//         //first I used .html() instead of .append() but html was just placing the last item
+//         if (el === 'fizzbuzz') {
+//             return $('.js-results').append(`<div class="fizz-buzz-item fizzbuzz"> <span>${el}</span> </div>`);
+//         } else if (el === 'buzz') {
+//             return $('.js-results').append(`<div class="fizz-buzz-item buzz"> <span>${el}</span> </div>`);
+//         } else if (el === "fizz") {
+//             return $('.js-results').append(`<div class="fizz-buzz-item fizz"> <span>${el}</span> </div>`);
+//         } else {
+//             return $('.js-results').append(`<div class="fizz-buzz-item"> <span>${el}</span> </div>`);
+//         }
+//     });
+// };
+//refactor
 let getFizzBuzzRange = arr => {
 
     return arr.map((el, ind) => {
         if (el % 15 === 0) {
-            return "fizzbuzz";
+            return `<div class="fizz-buzz-item fizzbuzz"> <span>${strFizzBuzz}</span> </div>`;
         } else if (el % 5 === 0) {
-            return "buzz";
+            return `<div class="fizz-buzz-item buzz"> <span>${strBuzz}</span> </div>`;
         } else if (el % 3 === 0) {
-            return "fizz";
+            return `<div class="fizz-buzz-item fizz"> <span>${strFizz}</span> </div>`;
         } else {
-            return el;
+            return `<div class="fizz-buzz-item"> <span>${el}</span> </div>`;
         }
+
     });
 };
 
 let pushFizzBuzzHTML = arr => {
-    return arr.map((el, ind) => {
-
-        //first I used .html() instead of .append() but html was just placing the last item
-        if (el === 'fizzbuzz') {
-            return $('.js-results').append(`<div class="fizz-buzz-item fizzbuzz"> <span>${el}</span> </div>`);
-        } else if (el === 'buzz') {
-            return $('.js-results').append(`<div class="fizz-buzz-item buzz"> <span>${el}</span> </div>`);
-        } else if (el === "fizz") {
-            return $('.js-results').append(`<div class="fizz-buzz-item fizz"> <span>${el}</span> </div>`);
-        } else {
-            return $('.js-results').append(`<div class="fizz-buzz-item"> <span>${el}</span> </div>`);
-        }
-    });
-
+    return $('.js-results').append(arr);
 };
 
 $('#number-chooser').on('click', '#btn-submit', event => {
